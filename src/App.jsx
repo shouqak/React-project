b22df5a3846446c7b1e7d24debda51325021fb67
shouq <shouqbis@gmail.com>
2025-05-23 10:59:51 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+import Router from './Router/Router'
+function App() {
+
+  return (
+    <>
+        <Router></Router>
+
+    </>
+  )
+}
+
+export default App
